8dc0c077ac67bd0dee2ed4415346cd9507e89702
phamvannam <namcpf@gmail.com>
2014-06-17 20:13:03 +0700

    sssdd
    dddd

---
+++ UnZip/src/vn/cyber/nampack/BrachNam.java
@@ -0,0 +1,28 @@
+package vn.cyber.nampack;
+
+public class BrachNam {
+	String teo,ty;
+
+	public String getTeo() {
+		return teo;
+	}
+
+	public void setTeo(String teo) {
+		this.teo = teo;
+	}
+
+	public String getTy() {
+		return ty;
+	}
+
+	public void setTy(String ty) {
+		this.ty = ty;
+	}
+
+	public BrachNam(String teo, String ty) {
+		super();
+		this.teo = teo;
+		this.ty = ty;
+	}
+	
+}
