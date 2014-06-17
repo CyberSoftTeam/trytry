782e3bc971d5de0c3189ea0d923464667d16f38a
phamvannam <namcpf@gmail.com>
2014-06-17 19:56:47 +0700

    Addd uSer and Email
    Addd uSer and Email

---
+++ UnZip/src/vn/cyber/nampack/Email.java
@@ -0,0 +1,59 @@
+package vn.cyber.nampack;
+
+public class Email {
+	String name,mail;
+
+	public User(String name, String mail) {
+		super();
+		this.name = name;
+		this.mail = mail;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((mail == null) ? 0 : mail.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (mail == null) {
+			if (other.mail != null)
+				return false;
+		} else if (!mail.equals(other.mail))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	
+}
